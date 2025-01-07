1c39767aca0eb0909c39a9a70c04c79eca4c72f9
Vedaant Joshi <vedaant912@gmail.com>
2025-01-07 14:58:14 +0100

    Hyperparameter training

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -49,7 +49,45 @@ class ModelTrainer:
                 'AdaBoost Classifier': AdaBoostRegressor()
             }
 
-            model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test = X_test, y_test = y_test, models=models)
+            params = {
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                 
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "K-Neighbors Classifier":{},
+                "XGBClassifier":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Classifier":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Classifier":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+            }
+
+            model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test = X_test, y_test = y_test, models=models, param = params)
 
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -64,7 +102,7 @@ class ModelTrainer:
             if best_model_score < 0.6:
                 raise CustomException("No best model found.")
             
-            logging.info(f'Best found model on both training and testing dataset')
+            logging.info(f'Best found model on both training and testing dataset : {best_model}')
 
             save_object(
                 file_path = self.model_trainer_config.trained_model_file_path,
